941d7f0c472f2a528084f0a9e09d76fb033b01d5
fedulovs <heaven45@mail.ru>
2024-10-04 16:30:01 +0300

    clean up imports

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,5 @@
 'use client';
 
-import Image from 'next/image';
-import styles from './page.module.css';
-import Card from './card/[categoryName]/page';
 import Categories from './categories/page';
 
 export default function Home() {
